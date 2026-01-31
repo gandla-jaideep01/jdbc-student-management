a4fadff579d3852ee0fc64aa92a43ba8093dbf45
gandla-jaideep01 <24211a05g4@bvrit.ac.in>
2026-01-31 16:44:26 +0530

    Update studentInsert.java

--- studentInsert.java
+++ studentInsert.java
@@ -10,7 +10,7 @@ public class studentInsert {
         Scanner sc = new Scanner(System.in);
 
         try {
-            // 1. Take input from user
+            
             System.out.print("Enter name: ");
             String name = sc.nextLine();
 
@@ -21,30 +21,30 @@ public class studentInsert {
             System.out.print("Enter roll: ");
             String roll = sc.nextLine();
 
-            // 2. Load driver
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // 3. Connect to MySQL
+            
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/classroom",
                     "root",
                     "Jesus@143");
 
-            // 4. SQL INSERT query
+            
             String sql = "INSERT INTO student (sname,sid, sroll) VALUES (?,?, ?)";
 
-            // 5. Prepare statement
+            
             PreparedStatement pst = con.prepareStatement(sql);
             pst.setString(1, name);
             pst.setInt(2, id);
             pst.setString(3, roll);
 
-            // 6. Execute
+            
             pst.executeUpdate();
 
             System.out.println("Data inserted successfully!");
 
-            // 7. Close connections
+            
             pst.close();
             con.close();
             sc.close();
